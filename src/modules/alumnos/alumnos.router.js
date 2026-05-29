5248352c6ec2493f0daf4707c47f3a5d2e863ed9
Omar Suniaga <omareduardo74@gmail.com>
2026-05-28 21:17:44 -0400

    fix(alumnos): mover inscripción de maestros a admin exclusivo

--- src/modules/alumnos/alumnos.router.js
+++ src/modules/alumnos/alumnos.router.js
@@ -1,8 +1,10 @@
 import { router } from '../../core/router/router.js'
 import { renderAlumnosView } from './views/alumnosView.js'
 import { renderReporteInscripcionesMes } from './views/reporteInscripcionesMes.js'
+import { renderWizardInscripcionAlumnoView } from '../../portal-maestros/views/wizardInscripcionAlumnoView.js'
 
 export function registerRoutesAlumnos() {
   router.register('alumnos', renderAlumnosView)
   router.register('alumnos-reporte-mes', renderReporteInscripcionesMes)
+  router.register('alumnos-inscribir', renderWizardInscripcionAlumnoView)
 }

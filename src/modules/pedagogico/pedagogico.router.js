55f9331e4b166ab98fabda302cffcf86551c6884
Omar Suniaga <omareduardo74@gmail.com>
2026-06-06 14:46:15 -0400

    feat(pedagogico): admin view for teacher needs solicitudes with filters and status management

--- src/modules/pedagogico/pedagogico.router.js
+++ src/modules/pedagogico/pedagogico.router.js
@@ -1,10 +1,12 @@
 import { router } from '../../core/router/router.js'
-import { renderDashboardPedagogicoView } from './views/dashboardPedagogicoView.js'
-import { renderSeguimientoAlumnosView }  from './views/seguimientoAlumnosView.js'
-import { renderReportesPedagogicosView } from './views/reportesPedagogicosView.js'
+import { renderDashboardPedagogicoView }  from './views/dashboardPedagogicoView.js'
+import { renderSeguimientoAlumnosView }   from './views/seguimientoAlumnosView.js'
+import { renderReportesPedagogicosView }  from './views/reportesPedagogicosView.js'
+import { renderSolicitudesAdminView }     from './views/solicitudesAdminView.js'
 
 export function registerRoutesPedagogico() {
   router.register('pedagogico-dashboard',   (c) => renderDashboardPedagogicoView(c))
   router.register('pedagogico-seguimiento', (c) => renderSeguimientoAlumnosView(c))
   router.register('pedagogico-reportes',    (c) => renderReportesPedagogicosView(c))
+  router.register('pedagogico-solicitudes', (c) => renderSolicitudesAdminView(c))
 }
